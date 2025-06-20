1aa9d5d8649a782df62d7b97ac6a076c77e6dc03
Ola Philips. <50849543+olabrass@users.noreply.github.com>
2025-06-21 00:36:09 +0100

    Production environment testing

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -13,7 +13,14 @@ const handleDuplicateFieldsDB = err => {
     return new AppError(message, 400);
 }
 
-// Dev error sender
+// Handling Validation error
+const handleValidationErrorDB = err => {
+    const errors = Object.values(err.errors).map(el => el.message);
+    const message = `Invalid input data. ${errors.join('. ')}`;
+    return new AppError(message, 400);
+};
+
+// Development error sender
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -49,13 +56,8 @@ module.exports = (err, req, res, next) => {
 
         if (error.name === 'CastError') error = handleCastErrorDB(error);
         if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+        if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
 
         sendErrorProd(error, res);
     }
-    //  else {
-    //     res.status(500).json({
-    //         status: 'error',
-    //         message: 'Something went very wrong!'
-    //     });
-    // }
 };
--- server.js
+++ server.js
@@ -4,12 +4,20 @@ const dotenv = require('dotenv');
 
 dotenv.config({path: './config.env'});
 
+// Handling Uncaught Exception, error from synchronous code
+process.on('uncaughtException', err => {
+    console.log('Uncaught Exception! Shutting down...');
+    console.log(err.name, err.message);
+        process.exit(1);
+});
+    
+
 // database connection
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD).replace('<DATABASE>', process.env.DATABASE_NAME);
 mongoose.connect(DB).then(con =>{
     console.log(con.connection);
     console.log('DB connected successfully');
-}).catch((error) => console.error('Error connecting to MongoDB:', error));
+});
 const port = process.env.PORT || 3000; 
 
 // Environment Variable
@@ -18,9 +26,15 @@ const port = process.env.PORT || 3000;
 
 
 // CREATING SERVER 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App runing on port ${port}`);
-    })
+    });
 
-
-    
+    // Handling Unhandled Rejection, error from promise rejection (Asyncronous code)
+    process.on('unhandledRejection', err => {
+        console.log('Unhandled Rejection! Shutting down...');
+        console.log(err.name, err.message);
+        server.close(() => {
+            process.exit(1);
+        });
+    });

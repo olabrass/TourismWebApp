97b87324ed4a31c3ad6a2b9f19b430dc7552acf1
Ola Philips. <50849543+olabrass@users.noreply.github.com>
2025-07-29 11:44:16 +0100

    Geospatial Route
    Finding tour within a specific area

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -16,10 +16,44 @@ router.param('id', tourController.checkId);
 
 // REFACTORING ROUTE FOR TOURS
 router.route('/tour-stats').get(tourController.getTourStats);
-router.route('/monthly-plan/:year').get(authController.protect, authController.restrictTo('admin', 'lead-guide', 'guide'),tourController.getMonthlyPlan);
-router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTour);
-router.route('/').get(tourController.getAllTour).post(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.createTour);
-router.route('/:id').get(tourController.getTourById).patch(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.updateTour).delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.deleteTour);     
+
+router.route('/monthly-plan/:year')
+.get(
+    authController.protect, 
+    authController.restrictTo('admin', 'lead-guide', 'guide'),
+    tourController.getMonthlyPlan
+);
+
+router.route('/top-5-cheap')
+.get(
+    tourController.aliasTopTours, 
+    tourController.getAllTour
+);
+
+
+router.route('/tours-within/:distance/center/:latlng/unit/:unit')
+.get(tourController.getToursWithin);
+
+router.route('/')
+.get(tourController.getAllTour)
+.post(
+    authController.protect, 
+    authController.restrictTo('admin', 'lead-guide'), 
+    tourController.createTour
+);
+
+router.route('/:id')
+.get(tourController.getTourById)
+.patch(
+    authController.protect, 
+    authController.restrictTo('admin', 'lead-guide'), 
+    tourController.updateTour
+    )
+    .delete(
+        authController.protect, 
+        authController.restrictTo('admin', 'lead-guide'), 
+        tourController.deleteTour
+    );     
 
 
 
